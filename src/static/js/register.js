1a2077a6a9c707b80158d0656a40ec04a88715dd
HarborYuan <yuanhaobo@whu.edu.cn>
2018-05-12 23:38:50 +0800

    register.js and go to sleep

--- src/static/js/register.js
+++ src/static/js/register.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
   $('select').formSelect();
 });
+
+$("#submit").click(function(){
+  let username = $("#username").val();
+  let idcard = $("#idcard").val();
+  let password1 = $("#password1").val();
+  let password2 = $("#password2").val();
+  let major = $("#major").val();
+  let email = $("#email").val();
+  let phone = $("#phone").val();
+  let isGirl = $("#isGirl").prop("checked");
+  console.log(username);
+  console.log(idcard);
+  console.log(password1);
+  console.log(password2);
+  console.log(major);
+  console.log(email);
+  console.log(phone);
+  console.log(isGirl);
+});
